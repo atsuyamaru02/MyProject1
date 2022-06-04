91326f49df1e71d148cce4f493bf331aaecd00db
keioh <keioh@192.168.2.110>
2022-06-04 16:24:11 +0900

    first commit

---
+++ src/main/java/com/example/demo/VendingMachine1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachine1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingMachine1Application.class, args);
+	}
+
+}
